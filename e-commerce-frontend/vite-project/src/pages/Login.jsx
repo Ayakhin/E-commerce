55a2ca1d15dabb1fd858dcf0e2510483bd6add85
Ayakhinm <rayanhilliman@egmail.com>
2024-07-16 14:34:48 +0200

    Fix de bug sur la connexion

--- e-commerce-frontend/vite-project/src/pages/Login.jsx
+++ e-commerce-frontend/vite-project/src/pages/Login.jsx
@@ -8,7 +8,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("/api/login", { email, password });
+      const response = await axios.post("http://localhost:5000/api/login", { email, password });
       const token = response.data.token;
       localStorage.setItem("token", token);
       alert("User logged in successfully");
